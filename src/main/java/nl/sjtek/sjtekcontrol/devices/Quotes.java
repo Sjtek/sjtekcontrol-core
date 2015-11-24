4a24ac5779f3873608e1b2e7b1dd33468366126b
Wouter Habets <wouterhabets@gmail.com>
2015-11-24 21:01:10 +0100

    Quotes bug fixes

--- src/main/java/nl/sjtek/sjtekcontrol/devices/Quotes.java
+++ src/main/java/nl/sjtek/sjtekcontrol/devices/Quotes.java
@@ -1,6 +1,7 @@
 package nl.sjtek.sjtekcontrol.devices;
 
 import nl.sjtek.sjtekcontrol.data.Settings;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.Random;
@@ -12,10 +13,24 @@ public class Quotes {
 
     private Random random = new Random();
 
+    public String getAll() {
+        JSONArray jsonArray = new JSONArray();
+        for (String quote : Settings.getInstance().getQuotes()) {
+            jsonArray.put(quote);
+        }
+        return jsonArray.toString();
+    }
+
     @Override
     public String toString() {
         String[] quotes = Settings.getInstance().getQuotes();
-        String quote = quotes[random.nextInt(quotes.length)];
+        String quote;
+        if (quotes.length > 0) {
+            quote = quotes[random.nextInt(quotes.length)];
+        } else {
+            quote = "";
+        }
+
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("quote", quote);
         return jsonObject.toString();
