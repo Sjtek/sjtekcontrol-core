2b16216332c07a61b4f9b9e3b4bafb82b2a5f55f
Wouter Habets <wouterhabets@gmail.com>
2016-01-08 18:22:16 +0100

    Auto disable night mode

--- src/main/java/nl/sjtek/sjtekcontrol/modules/NightMode.java
+++ src/main/java/nl/sjtek/sjtekcontrol/modules/NightMode.java
@@ -5,12 +5,24 @@ import nl.sjtek.sjtekcontrol.network.Arguments;
 import nl.sjtek.sjtekcontrol.utils.Speech;
 import org.json.JSONObject;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * Created by wouter on 28-11-15.
  */
 public class NightMode extends BaseModule {
 
     private boolean enabled = false;
+    private Timer timer;
+    private Date nextDisable = Calendar.getInstance().getTime();
+
+    public NightMode() {
+        this.timer = new Timer();
+        schedule();
+    }
 
     public void enable(Arguments arguments) {
         ApiHandler.getInstance().getMusic().pause(new Arguments());
@@ -31,11 +43,31 @@ public class NightMode extends BaseModule {
     public JSONObject toJson() {
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("enabled", enabled);
+        jsonObject.put("nextDisable", nextDisable.toString());
         return jsonObject;
     }
 
     @Override
     public String getSummaryText() {
         return "Night mode is " + (enabled ? "enabled" : "disabled") + ".";
+    }
+
+    private void schedule() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 8);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.add(Calendar.DAY_OF_YEAR, 1);
+        nextDisable = calendar.getTime();
+        timer.schedule(new DisableTask(), nextDisable);
+    }
+
+    private class DisableTask extends TimerTask {
+
+        @Override
+        public void run() {
+            enabled = false;
+            schedule();
+        }
     }
 }
