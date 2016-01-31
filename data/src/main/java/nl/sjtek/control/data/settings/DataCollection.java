b96c09b7a4fce03265e4a6957770ed52c98df675
Wouter Habets <wouterhabets@gmail.com>
2016-02-01 00:43:33 +0100

    Something with data collection (can't remember)

---
+++ data/src/main/java/nl/sjtek/control/data/settings/DataCollection.java
@@ -0,0 +1,34 @@
+package nl.sjtek.control.data.settings;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by wouter on 29-1-16.
+ */
+public class DataCollection {
+
+    private final Map<String, PlaylistSet> playlists;
+    private final Map<String, User> users;
+
+    public DataCollection(Map<String, PlaylistSet> playlists, Map<String, User> users) {
+        this.playlists = playlists;
+        this.users = users;
+    }
+
+    public DataCollection(Map<String, User> users) {
+        this.users = users;
+        this.playlists = new HashMap<>();
+        for (Map.Entry<String, User> entry : users.entrySet()) {
+            this.playlists.put(entry.getKey(), entry.getValue().getPlaylistSet());
+        }
+    }
+
+    public Map<String, PlaylistSet> getPlaylists() {
+        return playlists;
+    }
+
+    public Map<String, User> getUsers() {
+        return users;
+    }
+}
