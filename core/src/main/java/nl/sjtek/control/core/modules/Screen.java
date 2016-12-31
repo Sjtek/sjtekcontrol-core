2a151c1a0638d5017c491c1c737597c74620cbc4
Wouter Habets <wouterhabets@gmail.com>
2016-12-31 01:42:48 +0100

    Screen improvements

--- core/src/main/java/nl/sjtek/control/core/modules/Screen.java
+++ core/src/main/java/nl/sjtek/control/core/modules/Screen.java
@@ -1,8 +1,10 @@
 package nl.sjtek.control.core.modules;
 
+import nl.sjtek.control.core.events.Bus;
 import nl.sjtek.control.core.network.ApiHandler;
 import nl.sjtek.control.core.network.Arguments;
 import nl.sjtek.control.core.utils.Speech;
+import nl.sjtek.control.data.ampq.events.LightEvent;
 import nl.sjtek.control.data.responses.Response;
 import nl.sjtek.control.data.responses.ScreenResponse;
 
@@ -26,7 +28,7 @@ public class Screen extends BaseModule {
     private TimerTask timerTask = new TimerTask() {
         @Override
         public void run() {
-            triggered(null);
+//            triggered(null);
         }
     };
 
@@ -51,6 +53,7 @@ public class Screen extends BaseModule {
 
     public void tv(Arguments arguments) {
         this.state = ScreenResponse.State.TV;
+        ApiHandler.getInstance().getMusic().pause(new Arguments());
         dataChanged(true);
     }
 
@@ -73,7 +76,8 @@ public class Screen extends BaseModule {
         dataChanged(true);
         Speech.speakAsync("Triggered");
         Music music = ApiHandler.getInstance().getMusic();
-        ApiHandler.getInstance().getMusic().start(new Arguments("?noshuffle&url=" + playlist), false);
+        ApiHandler.getInstance().getMusic().start(new Arguments("noshuffle&url=" + playlist), false);
+        new DiscoThread().start();
     }
 
     @Override
@@ -84,5 +88,59 @@ public class Screen extends BaseModule {
     @Override
     public String getSummaryText() {
         return "Current state: " + state.toString();
+    }
+
+    private class DiscoThread extends Thread {
+        @Override
+        public void run() {
+            super.run();
+            int r = 255;
+            int g = 0;
+            int b = 0;
+
+            try {
+                for (int i = 0; i < 480; i++) {
+                    int step = i % 6;
+                    switch (step) {
+                        case 0:
+                            r = 255;
+                            g = 0;
+                            b = 0;
+                            break;
+                        case 1:
+                            r = 255;
+                            g = 255;
+                            b = 0;
+                            break;
+                        case 2:
+                            r = 0;
+                            g = 255;
+                            b = 0;
+                            break;
+                        case 3:
+                            r = 0;
+                            g = 255;
+                            b = 255;
+                            break;
+                        case 4:
+                            r = 0;
+                            g = 0;
+                            b = 255;
+                            break;
+                        case 5:
+                            r = 255;
+                            g = 0;
+                            b = 255;
+                            break;
+                    }
+                    Bus.post(new LightEvent(3, r, g, b));
+                    Thread.sleep(500);
+                }
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Bus.post(new LightEvent(3, 255, 150, 0));
+        }
     }
 }
