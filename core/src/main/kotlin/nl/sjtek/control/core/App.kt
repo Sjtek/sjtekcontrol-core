70accc858763bf61cebed963405f294d8c341df1
Wouter Habets <wouterhabets@gmail.com>
2017-08-28 23:34:00 +0200

    Add settings loading from disk

--- core/src/main/kotlin/nl/sjtek/control/core/App.kt
+++ core/src/main/kotlin/nl/sjtek/control/core/App.kt
@@ -1,7 +1,10 @@
 package nl.sjtek.control.core
 
+import nl.sjtek.control.core.settings.SettingsManager
+
 
 fun main(args: Array<String>) {
     println("Sjtek control core")
+    SettingsManager.settings
     ModuleManager.init()
 }
--- core/src/main/kotlin/nl/sjtek/control/core/settings/SettingsManager.kt
+++ core/src/main/kotlin/nl/sjtek/control/core/settings/SettingsManager.kt
@@ -1,5 +1,40 @@
 package nl.sjtek.control.core.settings
 
+import com.google.gson.GsonBuilder
+import org.slf4j.LoggerFactory
+import java.io.File
+
 object SettingsManager {
-    val settings = Settings()
+    val settings: Settings
+
+    init {
+        val logger = LoggerFactory.getLogger(javaClass)
+        logger.info("Loading settings")
+        val json: String = try {
+            File("/etc/sjtekcontrol-core/config.json").readText()
+        } catch (e: Exception) {
+            ""
+        }
+        val gson = GsonBuilder().setPrettyPrinting().create()
+        if (!json.isBlank()) {
+            var parsed: Settings? = null
+            try {
+                parsed = gson.fromJson<Settings>(json, Settings::class.java)
+            } catch (e: Exception) {
+                logger.error("Error loading settings", e)
+
+            }
+
+            if (parsed == null) {
+                parsed = Settings()
+                println("Missing settings, printing default ones")
+                println(gson.toJson(parsed))
+            }
+            settings = parsed
+        } else {
+            settings = Settings()
+            println("Missing settings, printing default ones")
+            println(gson.toJson(settings))
+        }
+    }
 }
