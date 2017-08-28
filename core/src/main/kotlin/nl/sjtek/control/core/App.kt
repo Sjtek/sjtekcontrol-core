3a863014eb7f6fad24e52979d22aabfdeb4b255b
Wouter Habets <wouterhabets@gmail.com>
2017-08-28 23:45:51 +0200

    Add bus error handler

--- core/src/main/kotlin/nl/sjtek/control/core/App.kt
+++ core/src/main/kotlin/nl/sjtek/control/core/App.kt
@@ -1,10 +1,16 @@
 package nl.sjtek.control.core
 
 import nl.sjtek.control.core.settings.SettingsManager
+import org.slf4j.LoggerFactory
+import kotlin.system.measureTimeMillis
 
 
 fun main(args: Array<String>) {
-    println("Sjtek control core")
-    SettingsManager.settings
-    ModuleManager.init()
+    val logger = LoggerFactory.getLogger("SjtekControl Core")
+    logger.info("Initialising...")
+    val initTime = measureTimeMillis {
+        SettingsManager.settings
+        ModuleManager.init()
+    }
+    logger.info("Sjtek control core started in ${initTime}ms")
 }
--- core/src/main/kotlin/nl/sjtek/control/core/events/Bus.kt
+++ core/src/main/kotlin/nl/sjtek/control/core/events/Bus.kt
@@ -1,12 +1,24 @@
 package nl.sjtek.control.core.events
 
 import net.engio.mbassy.bus.MBassador
+import net.engio.mbassy.bus.config.BusConfiguration
+import net.engio.mbassy.bus.config.Feature
+import net.engio.mbassy.bus.error.PublicationError
 import org.slf4j.LoggerFactory
 
 object Bus {
 
     private val logger = LoggerFactory.getLogger(javaClass)
-    private val bus = MBassador<Any>()
+    private val bus: MBassador<Event>
+
+    init {
+        val config = BusConfiguration()
+                .addPublicationErrorHandler(this::errorHandler)
+                .addFeature(Feature.SyncPubSub.Default())
+                .addFeature(Feature.AsynchronousHandlerInvocation.Default())
+                .addFeature(Feature.AsynchronousMessageDispatch.Default())
+        bus = MBassador(config)
+    }
 
     fun post(event: Event) {
         bus.publish(event)
@@ -18,5 +30,9 @@ object Bus {
 
     fun unsubscribe(subscriber: Any) {
         bus.unsubscribe(subscriber)
+    }
+
+    private fun errorHandler(error: PublicationError) {
+        logger.error("Bus error", error)
     }
 }
