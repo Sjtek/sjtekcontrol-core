9430baac3efdd2668d90ffdaf5e461c2016207ac
Wouter Habets <wouterhabets@gmail.com>
2017-12-20 22:17:12 +0100

    Fix screen response

--- data/src/main/kotlin/nl/sjtek/control/data/response/Screen.kt
+++ data/src/main/kotlin/nl/sjtek/control/data/response/Screen.kt
@@ -1,6 +1,5 @@
 package nl.sjtek.control.data.response
 
 data class Screen(override val key: String, val video: Boolean) : Response() {
-    override val type: String
-        get() = javaClass.canonicalName
+    override val type: String = javaClass.canonicalName
 }
